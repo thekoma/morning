a18b7d9f897d7d80b5980715c680a1706cf2f78f
Andrea Cervesato <andrea.cervesato@gmail.com>
2022-02-14 00:16:17 +0100

    Creating a test

---
+++ .github/bin/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if ! type -p executable_name &>/dev/null; then
+    apt-get update; apt-get install -y jq
+fi
+docker-compose -f docker-compose.yml up -d
+try=0
+while [ $try -lt 12 ]; do
+  health=$(docker inspect "$(docker-compose ps -q morning)" |jq '.[0] | .State.Health.Status'|sed's/"//g')
+  if [ "${health}" == "healthy" ]; then
+    echo "Morning is healthy! Great Job!"
+    exit_var=0
+    break
+  else
+    ((try++))
+    sleep 5
+    docker-compose ps
+    docker-compose logs morning
+  fi
+  echo "Something is wrong. Check it out."
+  exit_var=1
+done
+docker-compore down -v -t 1 --remove-orphans
+exit $exit_var
