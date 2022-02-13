b1f4dce2da0b7f4dc4c70bf835f0d7b309de69a2
Andrea Cervesato <andrea.cervesato@gmail.com>
2022-02-14 00:44:19 +0100

    I like emoji

--- .github/bin/test.sh
+++ .github/bin/test.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
+
 docker-compose -f docker-compose.yml up -d
 try=0
+retry=12
+exit_var=1
+sleep=5
 echo "🚦 Starting checks."
-while [ $try -lt 12 ]; do
+while [ $try -lt $retry ]; do
   health=$(docker inspect "$(docker-compose ps -q morning)" |jq '.[0] | .State.Health.Status'|sed 's/"//g')
+  echo "🔍 Checking...[${try}/${retry}]"
   if [ "${health}" == "healthy" ]; then
-    echo "🌞 Morning is healthy! Great Job!"
     exit_var=0
     break
   else
     ((try++))
     docker-compose ps
     docker-compose logs morning
-    echo "⏳ Not ready. Sleeping"
-    sleep 5
+    echo "⏳ Not ready. Sleeping $sleep seconds. "
+    sleep $sleep
+
   fi
-  echo "🚨 Test failed"
-  exit_var=1
 done
+
+if [ $exit_var == 1 ]; then
+  echo "🚨 Test failed"
+else
+  echo "🌞 Morning is healthy! Great Job!"
+fi
+
 docker-compose down -v -t 1 --remove-orphans
 exit $exit_var
